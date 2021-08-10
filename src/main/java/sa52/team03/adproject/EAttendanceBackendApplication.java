5da788e53820b2b8a22d19013d5f5d65da40ead1
kyawmt <kmt@kmtkko.com>
2021-08-10 19:00:50 +0800

    Generate spring starter files for project

---
+++ src/main/java/sa52/team03/adproject/EAttendanceBackendApplication.java
@@ -0,0 +1,13 @@
+package sa52.team03.adproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAttendanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAttendanceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sa52/team03/adproject/EAttendanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sa52.team03.adproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAttendanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
