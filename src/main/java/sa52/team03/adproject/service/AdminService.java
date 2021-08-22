4e968ca23daad826d8fdf385b4a8f01f90252666
jwong93 <jwong93@hotmail.com>
2021-08-22 18:21:44 +0800

    update

--- src/main/java/sa52/team03/adproject/service/AdminService.java
+++ src/main/java/sa52/team03/adproject/service/AdminService.java
@@ -53,4 +53,5 @@ public interface AdminService {
 	public List<Schedule> getClassFutureSchedule(int classId);
 	
 	public void updateClassPredictedAttendanceRate(int classId) throws Exception;
+	
 }
