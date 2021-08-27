f7e8541df7533297a8cb75eab1465dde798dd7dd
jwong93 <jwong93@hotmail.com>
2021-08-27 12:58:21 +0800

    minor tweaks

--- src/main/java/sa52/team03/adproject/scheduler/Scheduler2.java
+++ src/main/java/sa52/team03/adproject/scheduler/Scheduler2.java
@@ -1,30 +1,43 @@
 package sa52.team03.adproject.scheduler;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import sa52.team03.adproject.domain.Class;
+import sa52.team03.adproject.repo.ClassRepository;
 import sa52.team03.adproject.service.AdminService;
 import sa52.team03.adproject.service.LecturerService;
 
 public class Scheduler2 extends TimerTask {
 	
 	@Autowired
-	LecturerService lecturerService;
+	private LecturerService lecturerService;
+	
+	@Autowired 
+	private ClassRepository crepo;
 	
 	@Autowired
 	AdminService adminService;
 	
 	public void run() {
-		try {
-			List<Integer> classIDs = lecturerService.getallClassID();
-			for (Integer i : classIDs)
-				adminService.updateClassPredictedAttendanceRate(i);
+		
+		List<Class> c = crepo.findAll();
+		List<Integer> ic = new ArrayList<>();
+		for (Class cc : c) {
+			ic.add(cc.getId());
 		}
-		catch (Exception e) {
-			System.out.println("Error happened");
+		for (Integer i : ic) {
+			try {
+				adminService.updateClassPredictedAttendanceRate(i);
+			}
+			catch (Exception e) {
+				System.out.println(e);
 		}
 	}
-
+	}
 }
+
+
